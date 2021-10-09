50a88dbb2181ee7df76e96d020c9eda616d956e8
suhpark <laztwo@hanmail.net>
2021-10-09 23:38:08 +0900

    5.2 색상별 투두리스트 개수 구하기

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b1c78a39b3034ebc7051"; }
+/******/ 		__webpack_require__.h = function() { return "c4ef8425e8360da2ac32"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo, useCallback } from "react";
 import styled from "styled-components";
 import palette from "../styles/palette";
 import { TodoType } from "../types/todo";
@@ -124,12 +124,104 @@ interface IProps {
 }
 
 const TodoList: React.FC<IProps> = ({ todos }) => {
+
+    //* 색깔 객체 구하기 1
+    const getTodoColorNums = useCallback(() => {
+        let red = 0;
+        let orange = 0;
+        let yellow = 0;
+        let green = 0;
+        let blue = 0;
+        let navy = 0;
+        todos.forEach((todo) => {
+        switch (todo.color) {
+            case "red":
+            red += 1;
+            break;
+            case "orange":
+            orange += 1;
+            break;
+            case "yellow":
+            yellow += 1;
+            break;
+            case "green":
+            green += 1;
+            break;
+            case "blue":
+            blue += 1;
+            break;
+            case "navy":
+            navy += 1;
+            break;
+            default:
+            break;
+        }
+        });
+
+        return {
+        red,
+        orange,
+        yellow,
+        green,
+        blue,
+        navy,
+        };
+    }, [todos]);
+
+    //* 색상별 투두 개수
+    // const todoColorNums = useMemo(getTodoColorNums, [todos]);
+
+    //* 객체의 문자열 인덱스 사용을 위한 타입
+    type ObjectIndexType = {
+        [key: string]: number | undefined;
+    };
+
+    //* 색깔 객체 구하기 2
+    const todoColorNums = useMemo(() => {
+        const colors: ObjectIndexType = {};
+        todos.forEach((todo) => {
+        const value = colors[todo.color];
+        if (!value) {
+            //* 존재하지않던 key라면
+            colors[`${todo.color}`] = 1;
+        } else {
+            //* 존재하는 키라면
+            colors[`${todo.color}`] = value + 1;
+        }
+        });
+        return colors;
+    }, [todos]);
+    console.log(todoColorNums);
+
     return (
         <Container>
             <div className="todo-list-header">
                 <p className="todo-list-last-todo">
                     남은 TODO<span>{todos.length}개</span>
                 </p>
+                <div className="todo-list-header-colors">
+                    {Object.keys(todoColorNums).map((color, index) => (
+                        <div className="todo-list-header-color-num" key={index}>
+                            <div className={`todo-list-header-round-color bg-${color}`} />
+                            <p>{todoColorNums[color]}개</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="todo-list">
+                {todos.map((todo) => (
+                    <li className="todo-item" key={todo.id}>
+                        <div className="todo-left-side">
+                            <div className={`todo-color-block bg-${todo.color}`} />
+                            <p className={`todo-text ${todo.checked ? "checked-todo-text" : ""}`}>
+                                {todo.text}
+                            </p>
+                        </div>
+                        <div className="todo-right-side">
+                            {!todo.checked && (<button type="button" className="todo-button" onClick={() => {}} /> )}
+                        </div>
+                    </li>
+                ))}
             </div>
         </Container>
     )
