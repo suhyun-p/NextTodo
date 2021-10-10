eae04e2305722ede508b92b494160a244585d9ca
suhpark <laztwo@hanmail.net>
2021-10-10 15:17:18 +0900

    axios 설정하기

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a338895ddfea94ffa46a"; }
+/******/ 		__webpack_require__.h = function() { return "0d9d4fbeecf79842cb9a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { GetServerSideProps, NextPage } from "next";
 import TodoList from "../components/TodoList";
 import Axios from "Axios";
 import { TodoType } from "../types/todo";
+import { getTodosAPI } from "../lib/api/todo";
 
 const todos: TodoType[] = [
     { id:1, text:"마트 가서 장보기", color: "red", checked: false },
@@ -19,7 +20,7 @@ const app: NextPage = () => {
 
 export const getServerSideProps: GetServerSideProps = async () => {
     try {
-        const { data } = await Axios.get<TodoType[]>("http://localhost:3000/api/todos");
+        const { data } = await getTodosAPI();
         console.log(data);
         return { props: {}};
     } catch (e) {
